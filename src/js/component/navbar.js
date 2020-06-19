2b24151a5cef766a097a103846e7df72282d22e0
Chris <christianOlmos@protonmail.com>
2020-06-19 23:28:09 +0000

    first button in navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
+	const [dropDown, setDropDown] = useState(false);
+
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1">BabyName</span>
 			</Link>
 			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+				<button className="btn btn-primary" onClick={() => setDropDown(true)}>
+					Menu ?Burger
+				</button>
 			</div>
+
+			{dropDown ? (
+				<div>
+					<button>Live Video</button>
+					<button>Settings</button>
+					<button>History</button>
+				</div>
+			) : null}
 		</nav>
 	);
 };
