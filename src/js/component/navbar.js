69a84f57fba2ac756c4c74ee173504331e2113a6
Chris <christianOlmos@protonmail.com>
2020-06-20 16:42:12 +0000

    drop down open and closes onclick

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,15 +3,15 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	const [dropDown, setDropDown] = useState(false);
-
+	const toggleUpOrDrop = () => setDropDown(!dropDown);
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
 				<span className="navbar-brand mb-0 h1">BabyName</span>
 			</Link>
 			<div className="ml-auto">
-				<button className="btn btn-primary" onClick={() => setDropDown(true)}>
-					Menu ?Burger
+				<button className="btn btn-primary" onClick={toggleUpOrDrop}>
+					Menu
 				</button>
 			</div>
 
